5e0bd236705470e5f2f579e4018b12ec429dc8e3
David <davidalejandroaguilar@gmail.com>
2016-05-14 17:27:31 +0000

    Add cart logic

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,4 +1,19 @@
 class CartController < ApplicationController
-  def index
+  
+  def add
+    # id is the product id via 'Add to cart' & routes
+    id = params[:id] 
+    cart[id] ? cart[id] + 1 : cart[id] = 1
+    redirect_to :back
   end
+  
+  def clearCart
+    session[:cart] = nil
+    redirect_to action: :index
+  end
+  
+  def index
+    @cart = cart
+  end
+ 
 end
