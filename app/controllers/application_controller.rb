d86a10eb3d4769320e948aef3cd11a93f1b68ea7
David <davidalejandroaguilar@gmail.com>
2016-05-20 00:50:08 +0000

    Fix cart logic and add comments to code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,13 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
+  #Makes cart available in views.
   helper_method :cart
+  
+  # Ensure cart session hash exists, if not, it creates a new one with its Value to a blank hash.
   
   def cart  
     session[:cart] ||= {}
   end
-  
-  # Ensure cart exists and make it an array.
   
 end
--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,9 +1,13 @@
 class CartController < ApplicationController
   
+  # id is the product id via 'Add to cart' & routes.
+  # We're creating a cart session hash with key (product id) and value (quantity 1 or +1). This creates ["1", 1], ["2", 1], ["3", 2] etc.
+  # cart is defined at the Application Controller.
+  
   def add
-    # id is the product id via 'Add to cart' & routes
     id = params[:id] 
-    cart[id] ? cart[id] + 1 : cart[id] = 1
+    cart[id] ? cart[id] = cart[id] + 1 : cart[id] = 1 
+    flash[:notice] = "Added to cart"
     redirect_to :back
   end
   
