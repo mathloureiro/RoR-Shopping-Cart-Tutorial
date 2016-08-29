d9badc9a6c93b2ffafb86edbd238c0bdf2a3b3e4
Matheus Loureiro <web@matheusloureiro.com.br>
2016-08-28 22:38:12 -0300

    Fix cart undefined method or variable

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,6 @@
 class CartController < ApplicationController
   
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_cart
   
   # id is the product id via 'Add to cart' & routes.
   # We're creating a cart session hash with key (product id) and value (quantity 1 or +1). This creates ["1", 1], ["2", 1], ["3", 2] etc.
@@ -8,18 +8,23 @@ class CartController < ApplicationController
   
   def add
     id = params[:id] 
+    cart = @cart
     cart[id] ? cart[id] = cart[id] + 1 : cart[id] = 1 
     flash[:notice] = "Added to cart"
     redirect_to :back
   end
   
   def clearCart
-    session[:cart] = nil
+    @cart = nil
     redirect_to action: :index
   end
   
   def index
-    @cart = cart
   end
+
+  private
+    def set_cart
+      @cart = session[:cart]
+    end
  
 end
